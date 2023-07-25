1748e8e42bf5516c6b8189ca5af51f3a2befc058
TAPAN BISWAS <biswasdebosmita1701@gmail.com>
2023-07-25 21:34:25 +0530

    Created By Debosmita

---
+++ main.py
@@ -0,0 +1,56 @@
+def printBoard(xState,zState):
+    zer   = 'X' if xState[0] else('O' if zState[0] else 0)
+    one   = 'X' if xState[1] else('O' if zState[1] else 0)
+    two   = 'X' if xState[2] else('O' if zState[2] else 0)
+    thr   = 'X' if xState[3] else('O' if zState[3] else 0)
+    fou   = 'X' if xState[4] else('O' if zState[4] else 0)
+    fiv   = 'X' if xState[5] else('O' if zState[5] else 0)
+    six   = 'X' if xState[6] else('O' if zState[6] else 0)
+    sev   = 'X' if xState[7] else('O' if zState[7] else 0)
+    eig   = 'X' if xState[8] else('O' if zState[8] else 0)
+    #print(f"|-----|-----|-----|")
+    print(f"|{zer}|{one}|{two}|")
+    #print(f"|-----|-----|-----|")
+    print(f"|{thr}|{fou}|{fiv}|")
+    #print(f"|-----|-----|-----|")
+    print(f"|{six}|{sev}|{eig}|")
+    #print(f"|-----|-----|-----|")
+    pass
+def sum(a,b,c):
+    return a+b+c
+
+def checkWin(xState , zState):
+    wins=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    for win in wins:
+        if(sum(xState[win[0]],xState[win[1]],xState[win[2]])==3):
+            print("X wins")
+            return 1
+        if(sum(zState[win[0]],zState[win[1]],zState[win[2]])==3):
+            print("Zero wins")
+            return 0
+    return -1
+
+    
+if __name__=="__main__":
+    xState=[0,0,0,0,0,0,0,0,0]
+    zState=[0,0,0,0,0,0,0,0,0]
+    turn =1 #1 for x and zero for 0
+    print("Welcome to Tic Tac Toe")
+    printBoard(xState,zState)
+    while (True):
+        if (turn==1):
+            print("x's turn")
+            value=int(input("Please enter a value : "))
+            xState[value-1]=1
+        else:
+            print("O's turn")
+            value=int(input("Please enter a value : "))
+            zState[value-1]=1
+        check=checkWin(xState,zState)
+        if(check != -1):
+            printBoard(xState,zState)
+            print ("Match Over")
+            break
+        turn= 1-turn
+        printBoard(xState,zState)
+        
